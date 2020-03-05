96923c7f86b8f19160e7e10ca9eee7b2526a0772
Luan Carlos M.A <luancma18@gmail.com>
2020-03-05 13:27:36 +0000

    Created the layouts to auth and dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,6 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route component={() => <h1>404</h1>} />
     </Switch>
   );
